4fd3a7acf672ce1cf4f10f91fb26af6251e32abd
aplaraujo <aplaraujo@gmail.com>
2020-04-17 18:11:20 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  return (
+    <div>Oi</div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
